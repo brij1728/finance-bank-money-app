92f15804418e8f403188282958f7d035781958d0
Brijesh Kumar <bk10895@gmail.com>
2024-06-02 23:09:48 +0530

    feat: enhance Header component responsiveness and styling
    - Make Header component more responsive by introducing flex-wrap and adjusting padding, gaps and font sizes for different breakpoints
    - Improve logo display and button styles for better visibility and alignment on smaller screens
    - Adjust text styling and size for better readability across different devices

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -16,23 +16,32 @@ export const Header = () => {
 
   return (
     <header className="py-4">
-      <div className="container mx-auto flex justify-between items-center font-clash-display text-black w-full h-[60px]">
-        <div className="flex gap-10 items-center">
-          <div className="flex gap-3 items-center">
-            <Image src="/uifryLogo.png" alt="Uifry" width={30} height={30} />
+      <div className="container mx-auto flex flex-wrap justify-between items-center font-clash-display text-black w-full h-auto md:h-[60px]">
+        <div className="flex items-center gap-4 md:gap-10">
+          <div className="flex gap-1 md:gap-4">
+            <Image
+              src="/uifryLogo.png"
+              alt="Uifry"
+              width={24}
+              height={24}
+              className="w-4 h-4 md:w-8 md:h-8"
+            />
             <div className="relative flex items-center">
-              <span className="text-4xl font-bold">uifry</span>
-              <span className="absolute top-0 right-0 text-[6px] transform translate-x-2.5 translate-y-3.5 ">
+              <span className="text-base md:text-4xl font-normal md:font-bold">
+                uifry
+              </span>
+              <span className="absolute top-0 right-0 text-[4px] md:text-[6px] transform translate-x-1.5 md:translate-x-2.5 translate-y-2.5 md:translate-y-3.5">
                 TM
               </span>
             </div>
           </div>
+
           <nav>
-            <ul className="flex space-x-6 font-medium text-xl">
+            <ul className="flex items-center space-x-1.5 md:space-x-4 lg:space-x-6 font-light md:font-medium text-sm md:text-xl">
               {menu.map((item) => (
                 <li
                   key={item.name}
-                  className={`cursor-pointer ${selected === item.name ? 'text-[#FF5555] font-bold' : 'text-black'}`}
+                  className={`cursor-pointer ${selected === item.name ? 'text-[#FF5555] font-light md:font-bold' : 'text-black'}`}
                   onClick={() => setSelected(item.name)}
                 >
                   {item.name}
@@ -41,7 +50,7 @@ export const Header = () => {
             </ul>
           </nav>
         </div>
-        <button className="bg-black text-white text-[18px] leading-[28px] py-3 px-6 font-semibold rounded-sm">
+        <button className="bg-black text-white text-sm md:text-[18px] leading-4 md:leading-[28px] py-1 md:py-3 px-2 md:px-6 font-normal md:font-semibold rounded">
           Download
         </button>
       </div>
