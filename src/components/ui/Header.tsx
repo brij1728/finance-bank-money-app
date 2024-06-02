026d91783bd3ec787faa41dfdf553b57cfd380fc
Brijesh Kumar <bk10895@gmail.com>
2024-06-02 16:52:15 +0530

    style: enhance Header component layout and styling
    - Increase padding on header for better spacing
    - Adjust flex gap and alignment for logo and title
    - Refactor navigation menu for improved readability
    - Update button styling for a more prominent call-to-actio

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -15,31 +15,33 @@ export const Header = () => {
   ];
 
   return (
-    <header className="py-4 bg-white shadow ">
-      <div className="container mx-auto flex justify-between items-center font-clash-display text-black">
-        <div className="flex gap-3 justify-center items-center">
-          <Image src="/uifryLogo.png" alt="Uifry" width={30} height={30} />
-          <div className="relative flex items-center">
-            <span className="text-4xl font-bold">uifry</span>
-            <span className="absolute top-0 right-0 text-[6px] transform translate-x-2.5 translate-y-3.5 ">
-              TM
-            </span>
+    <header className="py-10 ">
+      <div className="container mx-auto flex justify-between items-center font-clash-display text-black w-full h-[60px]">
+        <div className="flex gap-10 justify-center items-center ">
+          <div className="flex gap-3 justify-center items-center">
+            <Image src="/uifryLogo.png" alt="Uifry" width={30} height={30} />
+            <div className="relative flex items-center">
+              <span className="text-4xl font-bold">uifry</span>
+              <span className="absolute top-0 right-0 text-[6px] transform translate-x-2.5 translate-y-3.5 ">
+                TM
+              </span>
+            </div>
           </div>
+          <nav>
+            <ul className="flex space-x-4 font-medium text-xl">
+              {menu.map((item) => (
+                <li
+                  key={item.name}
+                  className={`cursor-pointer ${selected === item.name ? 'text-[#FF5555] font-bold text-xl' : 'text-black font-medium'}`}
+                  onClick={() => setSelected(item.name)}
+                >
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
-        <nav>
-          <ul className="flex space-x-4 font-medium text-xl">
-            {menu.map((item) => (
-              <li
-                key={item.name}
-                className={`cursor-pointer ${selected === item.name ? 'text-[#FF5555] font-bold text-xl' : 'text-black font-medium'}`}
-                onClick={() => setSelected(item.name)}
-              >
-                {item.name}
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <button className="btn-primary bg-black text-white text-[18px] p-2 px-6 rounded-sm">
+        <button className="btn-primary bg-black text-white text-[18px] leading-[28px] py-2 px-11 font-semibold rounded w-[180px] h-[60px]">
           Download
         </button>
       </div>
