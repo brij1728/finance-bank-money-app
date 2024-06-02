9ca55e2115272ba1b1bcc45fdcc05498917fd86e
Brijesh Kumar <bk10895@gmail.com>
2024-06-02 21:43:54 +0530

    style: improve Hero component UI and typography
    - Adjust Hero title font size and weight for better readability
    - Change Hero title from h1 to p using semantic HTML
    - Update paragraph text style - font color, weight and capitalization
    - Add margin-top to button container for better spacing

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -4,15 +4,16 @@ export const Hero = () => {
   return (
     <section className="">
       <div className="container mx-auto flex flex-col float-start">
-        <div className="text-[64px] font-bold text-black ">
-          <h1 className="">Make The Best</h1>
-          <h1 className="">Financial Decisions</h1>
+        <div className="text-[60px] font-extrabold text-black space-y-0 leading-[68px]">
+          <p className="">Make The Best</p>
+          <p className="">Financial Decisions</p>
         </div>
-        <p className="mt-4 text-lg">
-          Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet
+        <p className="mt-4 text-lg text-gray-500 font-normal capitalize">
+          Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet{' '}
+          <br />
           faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.
         </p>
-        <div className="flex gap-4">
+        <div className="flex gap-4 mt-6">
           <button className="btn-primary bg-black text-white text-[18px] leading-[28px] py-4 px-[23px] rounded font-medium h-[60px]">
             <span className="flex items-center gap-2">
               <p className="">Get Started</p>
